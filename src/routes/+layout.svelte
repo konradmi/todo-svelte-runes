ad9027e012a44bb1a9c6e86a0195cc13e0cf37de
Konrad Miekus <miekus.konrad@gmail.com>
2024-01-26 08:26:23 +0100

    todo app svelte runes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<div class='layout'>
+  <slot></slot>
+</div>
+
+<style>
+  .layout {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,109 @@
+<script lang='ts'>
+  type Filter = 'all' | 'active' | 'completed'
+
+  class Todo {
+    text = $state('')
+    done = $state(false)
+    id = crypto.getRandomValues(new Uint32Array(1))[0]
+    
+    constructor(text: string, done: boolean) {
+      this.text = text
+      this.done = done
+    }
+
+    toJSON() {
+      return {
+        text: this.text,
+        done: this.done,
+        id: this.id,
+      }
+    }
+  }
+
+  let todos = $state<Todo[]>([
+    new Todo('TODO 1', false),
+    new Todo('TODO 2', true),
+    new Todo('TODO 3', false),
+  ])
+
+  let filter = $state<Filter>('all')
+
+  const addTodo = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      const target  = e.target as HTMLInputElement
+      // todos = [...todos, new Todo(target.value, false)]
+      todos = [...todos, { text: target.value, done: false, id: crypto.getRandomValues(new Uint32Array(1))[0] } as Todo]
+      target.value = ''
+    }
+  }
+  const editTodo = (e: Event, id: number) => {
+    const todo = todos.find(todo => todo.id === id)!
+    todo.text = (e.target as HTMLInputElement).value
+  }
+
+  const onChange = (e: Event, id: number) => {
+    const todo = todos.find(todo => todo.id === id)!
+    todo.done = !todo.done
+  }
+
+  const filterTodos = (todos: Todo[], filter: Filter) => {
+    switch (filter) {
+      case 'active': 
+        return todos.filter(todo => todo.done === false)
+      case 'completed':
+        return todos.filter(todo => todo.done === true)
+      default:
+        return todos
+    }
+  }
+
+  let filteredTodos = $derived(filterTodos(todos, filter))
+</script>
+
+<div class='page'>
+  <div class='filters'>
+    <button onclick={() => filter = 'all'}>All</button>
+    <button onclick={() => filter = 'active'}>Active</button>
+    <button onclick={() => filter = 'completed'}>Completed</button>
+  </div>
+  <input class='inputTodo' onkeydown={addTodo} type='text' placeholder="Add todo">
+  <div class='todos'>
+    {#each filteredTodos as todo (todo.id)}
+      <div class='todo'>
+        <input onchange={e => onChange(e, todo.id)} type='checkbox' checked={todo.done}/>
+        <input oninput={e => editTodo(e, todo.id)} type='text' value={todo.text}/>
+      </div>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .filters {
+    margin-bottom: 1rem;
+  }
+
+  .inputTodo {
+    align-self: flex-end;
+    margin-bottom: 1rem;
+  }
+
+  .page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .todos {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 5rem;
+  }
+
+  .todo {
+    display: flex;
+    align-items: center;
+  }
+
+  .todo input[type='checkbox'] {
+    margin-right: 1rem;
+  }
+</style>
